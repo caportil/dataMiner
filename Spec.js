9e9846598358f598e871dc01f701b06de5bd6e84
caportil <caportil@gmail.com>
2017-05-11 23:48:51 -0700

    Add supported instructions tests

--- Spec.js
+++ Spec.js
@@ -1,4 +1,4 @@
-describe('Test Format', function() {
+describe('Formats Correctly', function() {
   let locateCAdata = SearchData(mockData, 'LOCATE', 'CA');
   it('Logs a response', function() {
     expect(locateCAdata.length).toBeDefined();
@@ -9,4 +9,17 @@ describe('Test Format', function() {
   it('Lists number of found companies in required format', function() {
     expect(locateCAdata.indexOf(`\nNumber of Companies:`)).toBeGreaterThan(-1);
   })
+});
+describe('Accepts Supported Instructions', function() {
+  it('Does not accept insufficient parameters', function() {
+    expect(() => SearchData(mockData)).toThrow();
+    expect(() => SearchData(mockData, 'find_before')).toThrow();
+  });
+  it('Accepts "locate" command', function() {
+    expect(() => SearchData(mockData, 'LOCATE', 'AL')).not.toThrow();
+  });
+  let randomYear = (Math.floor(Math.random()*10000)).toString();
+  it('Accepts "find_before" command', function() {
+    expect(() => SearchData(mockData, 'find_before', randomYear)).not.toThrow();
+  });
 });
